ad6041ae2c889502eef30973540681f1aad9cac8
Hunter W <hunterw@Hunters-MacBook-Pro.local>
2023-11-24 11:11:39 -0800

    motor_test

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,28 +1,116 @@
-void setup() {
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
-  // put your setup code here, to run once:
+#include <util/atomic.h> // For the ATOMIC_BLOCK macro
 
+#define ENCA 2 // YELLOW
+#define ENCB 3 // WHITE
+#define PWM 24
+#define IN2 22
+#define IN1 26
+
+volatile int posi = 0; // specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+long prevT = 0;
+float eprev = 0;
+float eintegral = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
+  
+  pinMode(PWM,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  
+  Serial.println("target pos");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(2, 200);
-  analogWrite(5, 200);
-  digitalWrite(3, LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(4,HIGH);
-  digitalWrite(7,HIGH);
-  delay(2000);
-  digitalWrite(3, HIGH);
-  digitalWrite(6,HIGH);
-  digitalWrite(4,LOW);
-  digitalWrite(7,LOW);
-  delay(2000);
+
+  // set target position
+  int target = 200;
+  //int target = 250*sin(prevT/1e6);
+
+  // PID constants
+  float kp = 5;
+  float kd = 0.25;
+  float ki = 0.0136;
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  prevT = currT;
+
+  // Read the position in an atomic block to avoid a potential
+  // misread if the interrupt coincides with this code running
+  // see: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+  int pos = 0; 
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    pos = posi;
+  }
+  
+  // error
+  int e = pos - target;
+
+  // derivative
+  float dedt = (e-eprev)/(deltaT);
+
+  // integral
+  eintegral = eintegral + e*deltaT;
+
+  // control signal
+  float u = kp*e + kd*dedt + ki*eintegral;
+
+  // motor power
+  float pwr = fabs(u);
+  if( pwr > 255 ){
+    pwr = 255;
+  }
+
+  // motor direction
+  int dir = 1;
+  if(u<0){
+    dir = -1;
+  }
+
+  // signal the motor
+  setMotor(dir,pwr,PWM,IN1,IN2);
 
 
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
+  if (target != pos){
+    target++;
+  }
+
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm,pwmVal);
+  if(dir == 1){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+  }  
+}
+
+void readEncoder(){
+  int b = digitalRead(ENCB);
+  if(b > 0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
 }
